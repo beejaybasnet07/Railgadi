f92e6a50eca501d7861baffdf1f47ecf7d3668a5
shreeshastha <shreeshashre@gmail.com>
2021-08-12 16:26:50 +0545

    filter managed

--- dataSearch/index.php
+++ dataSearch/index.php
@@ -25,19 +25,19 @@
     $sql = '';
     
     if($date != NULL && $name == NULL && $code == NULL){
-        $sql = "SELECT b.date, b.code, p.pname FROM book as b LEFT JOIN passenger as p ON p.id = b.pid WHERE b.date = '$date'";
+        $sql = "SELECT b.date, b.code, b.class, b.berth, b.seat, p.pname FROM book as b LEFT JOIN passenger as p ON p.id = b.pid WHERE b.date = '$date'";
     }else if($date == NULL && $name == NULL && $code != NULL){
-        $sql = "SELECT b.date, b.code, p.pname FROM book as b LEFT JOIN passenger as p ON p.id = b.pid WHERE b.code = $code";
+        $sql = "SELECT b.date, b.code, b.class, b.berth, b.seat, p.pname FROM book as b LEFT JOIN passenger as p ON p.id = b.pid WHERE b.code = $code";
     }else if($date == NULL && $name != NULL && $code == NULL){
-        $sql = "SELECT b.date, b.code, p.pname FROM book as b LEFT JOIN passenger as p ON p.id = b.pid WHERE p.pname = '$name'";
+        $sql = "SELECT b.date, b.code, b.class, b.berth, b.seat, p.pname FROM book as b LEFT JOIN passenger as p ON p.id = b.pid WHERE p.pname = '$name'";
     }else if($date != NULL && $name != NULL && $code == NULL){
-        $sql = "SELECT b.date, b.code, p.pname FROM book as b LEFT JOIN passenger as p ON p.id = b.pid WHERE b.date = '$date' && p.pname = '$name'";
+        $sql = "SELECT b.date, b.code, b.class, b.berth, b.seat, p.pname FROM book as b LEFT JOIN passenger as p ON p.id = b.pid WHERE b.date = '$date' && p.pname = '$name'";
     }else if($date != NULL && $name == NULL && $code != NULL){
-        $sql = "SELECT b.date, b.code, p.pname FROM book as b LEFT JOIN passenger as p ON p.id = b.pid WHERE b.date = '$date' && b.code = $code";
+        $sql = "SELECT b.date, b.code, b.class, b.berth, b.seat, p.pname FROM book as b LEFT JOIN passenger as p ON p.id = b.pid WHERE b.date = '$date' && b.code = $code";
     }else if($date == NULL && $name != NULL && $code != NULL){
-        $sql = "SELECT b.date, b.code, p.pname FROM book as b LEFT JOIN passenger as p ON p.id = b.pid WHERE p.pname = '$name' && b.code = $code";
+        $sql = "SELECT b.date, b.code, b.class, b.berth, b.seat, p.pname FROM book as b LEFT JOIN passenger as p ON p.id = b.pid WHERE p.pname = '$name' && b.code = $code";
     }else if($date != NULL && $name != NULL && $code != NULL){
-        $sql = "SELECT b.date, b.code, p.pname FROM book as b LEFT JOIN passenger as p ON p.id = b.pid WHERE b.date = '$date' && p.pname = '$name' && b.code = $code";
+        $sql = "SELECT b.date, b.code, b.class, b.berth, b.seat, p.pname FROM book as b LEFT JOIN passenger as p ON p.id = b.pid WHERE b.date = '$date' && p.pname = '$name' && b.code = $code";
     }
 
     $statement = $pdo->prepare($sql);
@@ -46,19 +46,21 @@
 
 <div class="container mb-5">
     <div class="row">
-        <div class="col-xl-3 col-md-6">
+        <div class="col-md-3">
             <div class="card card-stats bg-info">
                 <div class="card-body">
-                    <h5 class="card-title text-uppercase font-weight-bold mb-0 h5">Total Ticket</br>Booking Today</h5>
-                    <span class="fa fa-check pr-2"></span>
+                    <h5 class="card-title text-uppercase font-weight-bold mb-0">Total Ticket Booking Today</h5>
+                    <span class="fa fa-ticket pr-2"></span>
                     <span class="h2 font-weight-bold mb-0"><?= $count;?></span>
                 </div>
             </div>
-        </div> <div class="col-xl-3 col-md-6">
+        </div> 
+        <div class="col-md-3">
             <div class="card card-stats bg-info">
                 <div class="card-body">
-                    <h5 class="card-title text-uppercase font-weight-bold mb-0 h3">Total Users</h5>
-                    <span class="fa fa-users pr-2"></span>
+                    <h5 class="card-title text-uppercase font-weight-bold mb-0">Total Users</h5>
+                    <br> 
+                    <span class="fa fa-users pr-2 w3-display-left"></span>
                     <span class="h2 font-weight-bold mb-0"><?= $user_count;?></span>
                 </div>
             </div>
@@ -71,12 +73,12 @@
     <form method="post">
         <div class="row">
             <div class="form-group col-md-2">
-                <label for="date">Date</label>
-                <input type="date" name ="date" id="book_date" class="form-control">
-            </div>
-            <div class="form-group col-md-2">
                 <label for="code">Code</label>
                 <input type="code" name="code" id="code" class="form-control">
+            </div>
+            <div class="form-group col-md-2">
+                <label for="date">Date</label>
+                <input type="date" name ="date" id="book_date" class="form-control">
             </div>
             <div class="form-group col-md-2">
                 <label for="name">Name</label>
@@ -101,9 +103,12 @@
                 <thead>
                     <tr>
                         <th>S.N</th>
-                        <th>Date</th>
                         <th>Code</th>
+                        <th>Date</th>
                         <th>Name</th>
+                        <th>Class</th>
+                        <th>Berth</th>
+                        <th>Seat</th>
                     </tr>
                 </thead>
 
@@ -118,6 +123,9 @@
                                 <td><?= $date;?></td>
                                 <td><?= $code;?></td>
                                 <td><?= $pname;?></td>
+                                <td><?= $class;?></td>
+                                <td><?= $berth;?></td>
+                                <td><?= $seat;?></td>
                             </tr>
                         <?php 
                             }
